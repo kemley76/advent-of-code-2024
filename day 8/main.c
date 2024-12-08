b0e26bf92d8185f45b84e54baca81a1ea9d591ca
Kaden Emley <kaden.emley@gmail.com>
2024-12-08 15:23:53 -0500

    day 8 formatting and comments
    Signed-off-by: Kaden Emley <kaden.emley@gmail.com>

--- day 8/main.c
+++ day 8/main.c
@@ -7,50 +7,57 @@ struct Point {
     int y;
 };
 
+struct PointList {
+    int size;
+    int capacity;
+    struct Point* ptr;
+};
+
 int main() {
-    printf("Hello World!\n");
-    int capacity = 500;
-    //int width = 12;
-    int width = 50;
+    int capacity = 16;
+    // int width = 12; // sample input width
+    int width = 50; // actual input width
     int idx = 0;
     char** grid = malloc(capacity * sizeof(char*));
     while (1) {
-        char *buffer = NULL;
+        char* buffer = NULL;
         int read;
         size_t len;
         read = getline(&buffer, &len, stdin);
         if (-1 != read) {
             // how do we determine width??
-            //char* row = malloc(width * sizeof(char));
-            //memcpy(row, buffer, len);
-            //free(buffer);
             grid[idx++] = buffer;
-            printf("Hello %c", grid[idx - 1][0]);
-        } else {
+        }
+        else {
             break;
         }
+
+        // expand grid rows if needed
         if (idx == capacity) {
             capacity *= 2;
             grid = realloc(grid, capacity * sizeof(char*));
         }
     }
 
+    // declare and initialize space for tracking where antinodes are
     char evil[idx][width];
     char evil2[idx][width];
-    for (int i = 0; i < width; i++) {
+    for (int i = 0; i < idx; i++) {
         for (int j = 0; j < width; j++) {
             evil[i][j] = 0;
             evil2[i][j] = 0;
         }
     }
-    //evil = malloc(width * idx * sizeof(char));
-    struct Point* frequencies[256];
-    unsigned char counts[256];
-    unsigned char sizes[256];
+
+    struct PointList frequencies[256];
     for (int i = 0; i < 256; i++) {
-        frequencies[i] = malloc(10 * sizeof(struct Point));
-        counts[i] = 0;
-        sizes[i] = 10;
+        frequencies[i].ptr = malloc(10 * sizeof(struct Point));
+        if (frequencies[i].ptr == NULL) {
+            printf("ERROR ALLOCATING MEMORY FOR FREQUENCY %c", i);
+            return 1;
+        }
+        frequencies[i].size = 0;
+        frequencies[i].capacity = 10;
     }
 
     for (int i = 0; i < idx; i++) {
@@ -59,68 +66,67 @@ int main() {
             if (c == '.') {
                 continue;
             }
-            if (counts[c] >= sizes[c]) {
-                sizes[c] *= 2;
-                frequencies[c] = realloc(frequencies[c], sizes[c]);
-            } 
-            struct Point *point = &frequencies[c][counts[c]++];
+            struct PointList* list = &frequencies[c];
+            if (list->size > list->capacity) {
+                list->capacity *= 2;
+                list->ptr = realloc(list->ptr, list->capacity);
+            }
+            struct Point* point = &list->ptr[list->size++];
             point->y = i;
             point->x = j;
-            printf("(%d, %d) %d %c\n", point->x, i, counts[c], (char)c);
         }
     }
 
     int total = 0;
     int total2 = 0;
     for (int freq = 0; freq < 256; freq++) {
-        if (counts[freq] == 0) {
+        struct PointList* list = &frequencies[freq];
+        if (list->size == 0) {
             continue;
         }
 
-        printf("\nFrequency: %c\n", freq);
-        for (int ant1 = 0; ant1 < counts[freq]; ant1++) {
-            for (int ant2 = 0; ant2 < counts[freq]; ant2++) {
+        for (int ant1 = 0; ant1 < list->size; ant1++) {
+            for (int ant2 = 0; ant2 < list->size; ant2++) {
                 if (ant1 == ant2)
                     continue;
-                struct Point p1 = frequencies[freq][ant1];
-                struct Point p2 = frequencies[freq][ant2];
-                int endX = 2 * p1.x - p2.x;
-                int endY = 2 * p1.y - p2.y;
+                const struct Point* p1 = &list->ptr[ant1];
+                const struct Point* p2 = &list->ptr[ant2];
+
+                // part 1
+                int endX = 2 * p1->x - p2->x;
+                int endY = 2 * p1->y - p2->y;
                 if (evil[endY][endX] != 1) {
                     if (!(endX < 0 || endY < 0 || endX >= width || endY >= idx)) {
                         evil[endY][endX] = 1;
                         total++;
                     }
                 }
-                endX = p2.x;
-                endY = p2.y;
+
+                /*
+                part 2 - start at antenna p2 and find every place
+                in line with both p1 and p2 that doesn't have an antinode yet
+                */
+                endX = p2->x;
+                endY = p2->y;
                 while (!(endX < 0 || endY < 0 || endX >= width || endY >= idx)) {
                     if (evil2[endY][endX] != 1) {
                         evil2[endY][endX] = 1;
                         total2++;
                     }
-                    endY += p1.y - p2.y;
-                    endX += p1.x - p2.x;
+                    endY += p1->y - p2->y;
+                    endX += p1->x - p2->x;
                 }
-                //printf("(%d, %d) & (%d, %d) -> %d %d\n", p1.x, p1.y, p2.x, p2.y, endX, endY);
             }
         }
     }
-    int newCount = 0;
+
     for (int i = 0; i < idx; i++) {
         for (int j = 0; j < width; j++) {
-            if (evil[i][j] == 1) {
-                newCount++;
-                printf("0");
-            } else {
-                printf(".");
-            }
+            printf("%c", evil[i][j] ? 'X' : (evil2[i][j] ? '#' : '.'));
         }
         printf("\n");
     }
-    printf("Total: %d\n", total);
-    printf("Total2: %d\n", total2);
-    printf("Idx: %d\n", idx);
-    printf("Net tot: %d\n", newCount);
+    printf("Part 1 total: %d\n", total);
+    printf("Part 2 total: %d\n", total2);
     return 0;
 }
