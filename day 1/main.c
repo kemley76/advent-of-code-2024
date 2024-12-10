60bb4ea68cd7b06e8ed64150331d6199f14f9d4a
Kaden Emley <kaden.emley@gmail.com>
2024-12-09 21:05:57 -0500

    linting
    Signed-off-by: Kaden Emley <kaden.emley@gmail.com>

--- day 1/main.c
+++ day 1/main.c
@@ -9,32 +9,35 @@ list is repeated in the right list, it iis added to the total similarity score
 #include <stdlib.h>
 #include <math.h>
 
-void heapify(int*, int);
-void swap(int*, int*);
-void heapifyDown(int*, int, int);
-int popHeap(int*, int);
+void heapify(int *, int);
+void swap(int *, int *);
+void heapifyDown(int *, int, int);
+int popHeap(int *, int);
 
-int main() {
+int main()
+{
     int capacity = 16; // initial capacity of heaps, expands as needed
     int size = 0;
 
-    int* lList = malloc(capacity * sizeof(int));
-    int* rList = malloc(capacity * sizeof(int));
+    int *lList = malloc(capacity * sizeof(int));
+    int *rList = malloc(capacity * sizeof(int));
 
-    // collect left and right lists from stdin 
+    // collect left and right lists from stdin
     int left, right;
-    while (scanf("%d %d", &left, &right) != EOF) {
+    while (scanf("%d %d", &left, &right) != EOF)
+    {
         lList[size] = left;
         rList[size] = right;
         size++;
-        if (size == capacity) {
+        if (size == capacity)
+        {
             capacity *= 2;
             lList = realloc(lList, capacity * sizeof(int));
             rList = realloc(rList, capacity * sizeof(int));
         }
     }
 
-    // turn left and right lists in to min heaps for constant time 
+    // turn left and right lists in to min heaps for constant time
     // removal of smallest element
     heapify(lList, size);
     heapify(rList, size);
@@ -42,7 +45,8 @@ int main() {
     // Part 1
     int s = size;
     int totalDifference = 0;
-    while (s > 0) {
+    while (s > 0)
+    {
         // get nth smallest elements in left and right lists
         left = popHeap(lList, s);
         right = popHeap(rList, s);
@@ -52,14 +56,17 @@ int main() {
 
     // Part 2
     // Left and right lists are now sorted in descending order after part 1
-    int* lPtr = lList; 
-    int* rPtr = rList;
+    int *lPtr = lList;
+    int *rPtr = rList;
     int similarityScore = 0;
-    // use two pointers to find which elements of the left list also occur in 
+    // use two pointers to find which elements of the left list also occur in
     // the right list (and find how many times)
-    while (lPtr < lList + size && rPtr < rList + size) {
-        while (rPtr < rList + size && *lPtr <= *rPtr) {
-            if (*lPtr == *rPtr) {
+    while (lPtr < lList + size && rPtr < rList + size)
+    {
+        while (rPtr < rList + size && *lPtr <= *rPtr)
+        {
+            if (*lPtr == *rPtr)
+            {
                 similarityScore += *lPtr;
             }
             rPtr++;
@@ -74,34 +81,42 @@ int main() {
 }
 
 // construct a min heap from an array
-void heapify(int* arr, int size) {
+void heapify(int *arr, int size)
+{
     // work from second to last level and work to root
     int level = log2((double)size) - 1;
-    for (; level >=0; level--) { // loop through each level
+    for (; level >= 0; level--)
+    { // loop through each level
         int i = (1 << level) - 1;
         int end = i + (1 << level);
-        for (; i < end; i++) { // loop through each node in that level
+        for (; i < end; i++)
+        { // loop through each node in that level
             heapifyDown(arr, i, size);
         }
-    } 
+    }
 }
 
 // move element at index i down heap to ensure no smaller elements are below
-void heapifyDown(int* arr, int i, int size) {
-    while (i * 2 + 1 < size) { // heapify down until complete
+void heapifyDown(int *arr, int i, int size)
+{
+    while (i * 2 + 1 < size)
+    { // heapify down until complete
         int lIdx = i * 2 + 1;
         int rIdx = i * 2 + 2;
-        
+
         int smallIdx = i; // index of the smallest value of the 3
-        if (arr[smallIdx] > arr[lIdx]) { // is left child smaller?
+        if (arr[smallIdx] > arr[lIdx])
+        { // is left child smaller?
             smallIdx = lIdx;
         }
 
-        if (rIdx < size && arr[smallIdx] > arr[rIdx]) { // right child smaller?
+        if (rIdx < size && arr[smallIdx] > arr[rIdx])
+        { // right child smaller?
             smallIdx = rIdx;
         }
 
-        if (smallIdx == i) { // parent is already smallest, stop heapifying
+        if (smallIdx == i)
+        { // parent is already smallest, stop heapifying
             break;
         }
 
@@ -112,15 +127,17 @@ void heapifyDown(int* arr, int i, int size) {
 
 // returns element at top of the heap and moves to end
 // re-heapifies heap to maintian structure
-int popHeap(int* heap, int size) {
+int popHeap(int *heap, int size)
+{
     int smallest = heap[0];
     swap(heap, heap + size - 1);
     heapifyDown(heap, 0, size - 1);
     return smallest;
 }
 
-void swap(int* a, int* b) {
-    int temp = *a; 
+void swap(int *a, int *b)
+{
+    int temp = *a;
     *a = *b;
     *b = temp;
 }
--- day 8/main.c
+++ day 8/main.c
@@ -2,131 +2,156 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Point {
-    int x;
-    int y;
+struct Point
+{
+	int x;
+	int y;
 };
 
-struct PointList {
-    int size;
-    int capacity;
-    struct Point* ptr;
+struct PointList
+{
+	int size;
+	int capacity;
+	struct Point *ptr;
 };
 
-int main() {
-    int capacity = 16;
-    // int width = 12; // sample input width
-    int width = 50; // actual input width
-    int idx = 0;
-    char** grid = malloc(capacity * sizeof(char*));
-    while (1) {
-        char* buffer = NULL;
-        int read;
-        size_t len;
-        read = getline(&buffer, &len, stdin);
-        if (-1 != read) {
-            // how do we determine width??
-            grid[idx++] = buffer;
-        }
-        else {
-            break;
-        }
+int main()
+{
+	int capacity = 16;
+	// int width = 12; // sample input width
+	int width = 50; // actual input width
+	int idx = 0;
+	char **grid = malloc(capacity * sizeof(char *));
+	while (1)
+	{
+		char *buffer = NULL;
+		int read;
+		size_t len;
+		read = getline(&buffer, &len, stdin);
+		if (-1 != read)
+		{
+			// how do we determine width??
+			grid[idx++] = buffer;
+		}
+		else
+		{
+			break;
+		}
 
-        // expand grid rows if needed
-        if (idx == capacity) {
-            capacity *= 2;
-            grid = realloc(grid, capacity * sizeof(char*));
-        }
-    }
+		// expand grid rows if needed
+		if (idx == capacity)
+		{
+			capacity *= 2;
+			grid = realloc(grid, capacity * sizeof(char *));
+		}
+	}
 
-    // declare and initialize space for tracking where antinodes are
-    char evil[idx][width];
-    char evil2[idx][width];
-    for (int i = 0; i < idx; i++) {
-        for (int j = 0; j < width; j++) {
-            evil[i][j] = 0;
-            evil2[i][j] = 0;
-        }
-    }
+	// declare and initialize space for tracking where antinodes are
+	char evil[idx][width];
+	char evil2[idx][width];
+	for (int i = 0; i < idx; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			evil[i][j] = 0;
+			evil2[i][j] = 0;
+		}
+	}
 
-    struct PointList frequencies[256];
-    for (int i = 0; i < 256; i++) {
-        frequencies[i].ptr = malloc(10 * sizeof(struct Point));
-        if (frequencies[i].ptr == NULL) {
-            printf("ERROR ALLOCATING MEMORY FOR FREQUENCY %c", i);
-            return 1;
-        }
-        frequencies[i].size = 0;
-        frequencies[i].capacity = 10;
-    }
+	struct PointList frequencies[256];
+	for (int i = 0; i < 256; i++)
+	{
+		frequencies[i].ptr = malloc(10 * sizeof(struct Point));
+		if (frequencies[i].ptr == NULL)
+		{
+			printf("ERROR ALLOCATING MEMORY FOR FREQUENCY %c", i);
+			return 1;
+		}
+		frequencies[i].size = 0;
+		frequencies[i].capacity = 10;
+	}
 
-    for (int i = 0; i < idx; i++) {
-        for (int j = 0; j < width; j++) {
-            unsigned char c = grid[i][j];
-            if (c == '.') {
-                continue;
-            }
-            struct PointList* list = &frequencies[c];
-            if (list->size > list->capacity) {
-                list->capacity *= 2;
-                list->ptr = realloc(list->ptr, list->capacity);
-            }
-            struct Point* point = &list->ptr[list->size++];
-            point->y = i;
-            point->x = j;
-        }
-    }
+	for (int i = 0; i < idx; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			unsigned char c = grid[i][j];
+			if (c == '.')
+			{
+				continue;
+			}
+			struct PointList *list = &frequencies[c];
+			if (list->size > list->capacity)
+			{
+				list->capacity *= 2;
+				list->ptr = realloc(list->ptr, list->capacity);
+			}
+			struct Point *point = &list->ptr[list->size++];
+			point->y = i;
+			point->x = j;
+		}
+	}
 
-    int total = 0;
-    int total2 = 0;
-    for (int freq = 0; freq < 256; freq++) {
-        struct PointList* list = &frequencies[freq];
-        if (list->size == 0) {
-            continue;
-        }
+	int total = 0;
+	int total2 = 0;
+	for (int freq = 0; freq < 256; freq++)
+	{
+		struct PointList *list = &frequencies[freq];
+		if (list->size == 0)
+		{
+			continue;
+		}
 
-        for (int ant1 = 0; ant1 < list->size; ant1++) {
-            for (int ant2 = 0; ant2 < list->size; ant2++) {
-                if (ant1 == ant2)
-                    continue;
-                const struct Point* p1 = &list->ptr[ant1];
-                const struct Point* p2 = &list->ptr[ant2];
+		for (int ant1 = 0; ant1 < list->size; ant1++)
+		{
+			for (int ant2 = 0; ant2 < list->size; ant2++)
+			{
+				if (ant1 == ant2)
+					continue;
+				const struct Point *p1 = &list->ptr[ant1];
+				const struct Point *p2 = &list->ptr[ant2];
 
-                // part 1
-                int endX = 2 * p1->x - p2->x;
-                int endY = 2 * p1->y - p2->y;
-                if (evil[endY][endX] != 1) {
-                    if (!(endX < 0 || endY < 0 || endX >= width || endY >= idx)) {
-                        evil[endY][endX] = 1;
-                        total++;
-                    }
-                }
+				// part 1
+				int endX = 2 * p1->x - p2->x;
+				int endY = 2 * p1->y - p2->y;
+				if (evil[endY][endX] != 1)
+				{
+					if (!(endX < 0 || endY < 0 || endX >= width || endY >= idx))
+					{
+						evil[endY][endX] = 1;
+						total++;
+					}
+				}
 
-                /*
-                part 2 - start at antenna p2 and find every place
-                in line with both p1 and p2 that doesn't have an antinode yet
-                */
-                endX = p2->x;
-                endY = p2->y;
-                while (!(endX < 0 || endY < 0 || endX >= width || endY >= idx)) {
-                    if (evil2[endY][endX] != 1) {
-                        evil2[endY][endX] = 1;
-                        total2++;
-                    }
-                    endY += p1->y - p2->y;
-                    endX += p1->x - p2->x;
-                }
-            }
-        }
-    }
+				/*
+				part 2 - start at antenna p2 and find every place
+				in line with both p1 and p2 that doesn't have an antinode yet
+				*/
+				endX = p2->x;
+				endY = p2->y;
+				while (!(endX < 0 || endY < 0 || endX >= width || endY >= idx))
+				{
+					if (evil2[endY][endX] != 1)
+					{
+						evil2[endY][endX] = 1;
+						total2++;
+					}
+					endY += p1->y - p2->y;
+					endX += p1->x - p2->x;
+				}
+			}
+		}
+	}
 
-    for (int i = 0; i < idx; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%c", evil[i][j] ? 'X' : (evil2[i][j] ? '#' : '.'));
-        }
-        printf("\n");
-    }
-    printf("Part 1 total: %d\n", total);
-    printf("Part 2 total: %d\n", total2);
-    return 0;
+	for (int i = 0; i < idx; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			printf("%c", evil[i][j] ? 'X' : (evil2[i][j] ? '#' : '.'));
+		}
+		printf("\n");
+	}
+	printf("Part 1 total: %d\n", total);
+	printf("Part 2 total: %d\n", total2);
+	return 0;
 }
