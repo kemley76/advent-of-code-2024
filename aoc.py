55b94e4e1dff8f083ede5c115d4b0ea3e77eb5e8
Kaden Emley <kaden.emley@gmail.com>
2024-12-12 09:53:26 -0500

    add compiler warnings
    Signed-off-by: Kaden Emley <kaden.emley@gmail.com>

--- aoc.py
+++ aoc.py
@@ -14,11 +14,12 @@ if cmd == "make":
     os.popen("cp ../template.c ./main.c")
 elif cmd == "run":
     os.chdir(folder)
+    comp = "gcc ./main.c -lm -Wall -Wextra -Wconversion -o  main.exe" 
     if len(sys.argv) > 3:
         if sys.argv[3] == "input":
-            os.system(f"gcc ./main.c -lm -o main.exe; ./main.exe < input.txt")
+            os.system(f"{comp}; ./main.exe < input.txt")
         elif sys.argv[3] == "input2": 
-            os.system(f"gcc ./main.c -lm -o main.exe; ./main.exe < input2.txt")
+            os.system(f"{comp}; ./main.exe < input2.txt")
     else:
-        os.system(f"gcc ./main.c -lm -o main.exe; ./main.exe")
+        os.system(f"{comp}; ./main.exe")
 
